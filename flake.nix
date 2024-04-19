d695485021cbe8dd12fd8810c8151cf29dde36bf
Nyarlathotep <anhedonia@skiff.com>
2024-04-19 02:11:40 -0400

    feat: update inputs
    - Add `nixpkgs-stable` nixos-23.11

--- flake.nix
+++ flake.nix
@@ -2,19 +2,18 @@
   description = "Horrible beyond anything you can imagine — but wonderful.";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-stable, ... }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      pkgs-stable = import nixpkgs-stable { inherit system; };
       lib = pkgs.lib.extend
         (self: super:
           (import ./lib { inherit pkgs; lib = self; }));
@@ -28,11 +27,15 @@
 
       nixosConfigurations.apocrypha = nixpkgs.lib.nixosSystem {
         inherit system pkgs lib;
-        specialArgs = inputs;
+        specialArgs = {
+          inherit (inputs) home-manager;
+          inherit pkgs-stable;
+        };
         modules = [
           self.nixosModules.default
           ./hosts/apocrypha
           {
+            home-manager.extraSpecialArgs = { inherit pkgs-stable; };
             home-manager. useGlobalPkgs = true;
             home-manager.useUserPackages = true;
           }
