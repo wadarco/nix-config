fe21b2fd76e7536b8c1686ebf664926a171d0f0e
Nyarlathotep <anhedonia@skiff.com>
2025-03-26 18:44:36 -0400

    feat(services): add flatpak-repo

--- modules/services.nix
+++ modules/services.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   services = {
     flatpak.enable = true;
     openssh = {
@@ -11,11 +12,11 @@
     };
   };
 
-  # systemd.services.flatpak-repo = {
-  #   wantedBy = [ "multi-user.target" ];
-  #   path = [ pkgs.flatpak ];
-  #   script = ''
-  #     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  #   '';
-  # };
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 }
