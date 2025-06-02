4c566cbe97537edd19ae8cf13abc003adbca8ef4
Nyarlathotep <anhedonia@skiff.com>
2025-06-01 21:55:37 -0400

    feat!: remove xorg server

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -15,14 +15,9 @@ in
 
   config = mkIf cfg.enable {
     services = {
-      xserver = {
-        enable = true;
-      };
       displayManager.gdm.enable = true;
       desktopManager.gnome.enable = true;
-
       gnome.core-apps.enable = false;
-      xserver.excludePackages = [ pkgs.xterm ];
     };
     environment.systemPackages = with pkgs; [ gnomeExtensions.appindicator ];
   };
